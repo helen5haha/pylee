52119e6be840e26a50f7c650f88f2fb51c2d6c52
siyun <siyun@xzydeMacBook-Air-8.local>
2017-09-13 15:41:33 +0800

    [number]: ADD ReverseInteger

---
+++ number/ReverseInteger.py
@@ -0,0 +1,29 @@
+'''
+Reverse digits of an integer.
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+Have you thought about this?
+Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+Did you notice that the reversed integer might overflow?
+Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+Note to handle the negative number, because it might cause overflow, but it is not the case in Python
+'''
+
+def reverse(x):
+    if x >= 0:
+        leave = x
+        sign = 1
+    else:
+        leave = -x
+        sign = -1
+
+    result = 0
+    while 0 != leave:
+        result = result * 10 + leave % 10
+        leave /= 10
+    return result * sign
+
+x = -123
+reverse(x)
